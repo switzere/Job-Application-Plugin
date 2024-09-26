92e43ee864f0cb3ba8af3779fe643e1dfc1763b0
switzere <switzere@uoguelph.ca>
2024-09-26 06:18:46 -0400

    Added in lever support

--- extractors/lever.js
+++ extractors/lever.js
@@ -1,9 +1,25 @@
 export function extractLeverDetails() {
   // TODO: Implement extraction logic for Lever
+  
+  // Select the <h2> element within the <div class="posting-headline">
+  const jobTitleElement = document.querySelector('.posting-headline h2');
+  // Get the text content of the <h2> element
+  const jobTitle = jobTitleElement.textContent.trim();
+
+  const companyInfoElement = document.title
+  const companyInfo = companyInfoElement ? companyInfoElement : 'Company Info Not Found';
+
+  const jobDescriptionElement = document.querySelector('[data-qa="job-description"]');
+  const jobDescription = jobDescriptionElement ? jobDescriptionElement.innerText : 'Job Description Not Found';
+
+  const locationElement = document.querySelector('.location');
+  const location = locationElement ? locationElement.innerText : 'Location Not Found';
+
   return {
     jobTitle: jobTitle,
     companyInfo: companyInfo,
     url: window.location.href,
-    jobDescription: jobDescription
+    jobDescription: jobDescription,
+    postingSource: 'Lever'
   };
 }
